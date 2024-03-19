2bb05505e6efb5d4e87350532f2bdd30b61e5a84
Tref4r <tungttthe172215@fpt.edu.vn>
2024-03-19 16:16:25 +0700

    Có được không ?

--- Frontend/src/app/customer/customer.component.ts
+++ Frontend/src/app/customer/customer.component.ts
@@ -15,6 +15,8 @@ export class CustomerComponent implements OnInit {
   display=false;
   selectedValue:string;
   transactions;
+  aId: any;
+  accId: any;
 
   constructor(
     private customerservice:CustomerService,
